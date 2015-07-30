f6f7a008eefb15ba46b65b18b75c63c54a97f5f9
Courtney <courtney.mae.phillips@gmail.com>
2015-07-30 15:22:06 -0700

    Add first spec.

--- spec/ping-pong_spec.rb
+++ spec/ping-pong_spec.rb
@@ -15,3 +15,10 @@
 # it will replace any number divisible by 5 with the word "pong"
 # ie: 5, 10, 15, etc. will all be replaced by "pong" wherever they occur.
 #
+
+describe("Fixnum#ping_pong") do
+  it("will count up to the given number") do
+    numbers = 7.ping_pong()
+    expect(numbers.last).to(eq(7))
+  end
+end
