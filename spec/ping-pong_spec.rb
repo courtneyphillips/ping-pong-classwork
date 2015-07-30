8624a3da04151b0638cc0c31bac84a35241c46b8
Courtney <courtney.mae.phillips@gmail.com>
2015-07-30 15:20:26 -0700

    Initialize files.

---
+++ spec/ping-pong_spec.rb
@@ -0,0 +1,17 @@
+# plain English version of specs:
+
+# it will count up to the given number
+# ie: 7.pingpong() should result in an array of numbers ending at 7
+#
+# it will replace 3 with the word "ping"
+# ie: wherever the number 3 would occur, the word ping will occur instead.
+#
+# it will replace any number divisible by 3 with the word "ping"
+# ie: 3, 6, 9, etc. will all be replaced by "ping" wherever they occur.
+#
+# it will replace 5 with the word "pong"
+# ie: wherever the number 5 would occur, "pong" will occur instead.
+#
+# it will replace any number divisible by 5 with the word "pong"
+# ie: 5, 10, 15, etc. will all be replaced by "pong" wherever they occur.
+#
